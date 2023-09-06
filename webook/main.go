e0bb939f94050f678140ff44cae409cef22b8d94
Deng Ming <ming_flycash@qq.com>
2023-09-06 16:53:15 +0800

    配置

--- webook/main.go
+++ webook/main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
 	"net/http"
 )
 
 func main() {
+	// 当前目录的配置
+	viper.AddConfigPath("./config")
 	server := InitWebServer()
 	// 注册路由
 	server.GET("/hello", func(ctx *gin.Context) {
